365435372a7c8cfe822cd5271df0e24ac0db13b9
matt <roling@umich.edu>
2018-06-13 11:27:49 -0400

    example meteorjs

---
+++ imports/ui/components/slider/slider.js
@@ -0,0 +1,5 @@
+import './slider.html';
+
+$(document).ready(function(){
+    $('.slider').slider();
+    });
---
+++ imports/ui/pages/home/home.js
@@ -0,0 +1,8 @@
+import './home.html';
+
+import '../../components/front/front.js';
+import '../../components/frontimage/frontimage.js';
+import '../../components/info/info.js';
+import '../../components/slider/slider.js';
+import '../../components/promotable/promotable.js';
+import '../../components/footer/footer.js';
